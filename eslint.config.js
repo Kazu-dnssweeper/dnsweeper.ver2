06aeb298f25a7abd0e56fe2bcf79b1fd67cb0845
Kazu-dnsweeper <kazugon.19960924@gmail.com>
2025-08-17 10:06:29 +0900

    chore: add eslint.config.js for ESLint v9 flat config

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+// @ts-check
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import importPlugin from 'eslint-plugin-import';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**', '.tmp/**'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { sourceType: 'module', ecmaVersion: 'latest' },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+];
+
